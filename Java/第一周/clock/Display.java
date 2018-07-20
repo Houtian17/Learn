e2bf2074fa0eb642312200daa20a0191e74ee786
unknown <1033893991@qq.com>
2018-07-21 01:20:53 +0800

    update

---
+++ Java/第一周/clock/Display.java
@@ -0,0 +1,22 @@
+package clock;
+
+public class Display {
+    private int value = 0;
+    private int limit = 0;
+
+    public Display(int limit) {
+        this.limit = limit;
+    }
+
+    public void increase() {
+        value++;
+        if (value == limit) {
+            value = 0;
+        }
+    }
+
+    public int getValue(){
+        return value;
+    }
+
+}
