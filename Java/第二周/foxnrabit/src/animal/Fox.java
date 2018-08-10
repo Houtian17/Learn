cb3f5fa905aefff2938dac123d3b2c43d2164963
unknown <1033893991@qq.com>
2018-08-11 07:30:03 +0800

    update

---
+++ Java/第二周/foxnrabit/src/animal/Fox.java
@@ -0,0 +1,46 @@
+package animal;
+
+import cell.Cell;
+
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.awt.Color;
+
+
+public class Fox extends Animal implements Cell {
+    public Fox() {
+        super(20, 4);
+    }
+
+    @Override
+    public void draw(Graphics g, int x, int y, int size) {
+        int alpha = (int) ((1 - getAgePercent()) * 255);
+        g.setColor(new Color(0, 0, 0, alpha));
+        g.fillRect(x, y, size, size);
+    }
+
+
+    @Override
+    public Animal breed() {
+        Animal ret = null;
+        if (isBreedable() && Math.random() < 0.05) {
+            ret = new Fox();
+        }
+        return ret;
+    }
+
+    @Override
+    public String toString(){
+        return "Fox:"+super.toString();
+    }
+
+    @Override
+    public Animal feed(ArrayList<Animal> neighbour){
+        Animal ret=null;
+        if (Math.random()<0.2){
+            ret=neighbour.get((int)(Math.random()*neighbour.size()));
+            longerLife(2);
+        }
+        return ret;
+    }
+}
