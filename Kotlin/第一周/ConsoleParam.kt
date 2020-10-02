ab38445a2cbf8ac651357f7f2583c2b2b2fd0033
kristen <1033893991@qq.com>
2020-10-02 15:16:08 +0800

    updata

--- Kotlin/第一周/ConsoleParam.kt
+++
@@ -1,5 +0,0 @@
-package xyz.houtian17.kt03
-
-fun main(args: Array<String>):Unit {
-    args.map(::println)
-}
--- Kotlin/第一周/ConsoleParam2.kt
+++
@@ -1,9 +0,0 @@
-package xyz.houtian17.kt03
-
-fun main(args: Array<String>) {
-    args.flatMap {
-        it.split("_")
-    }.map {
-        print("$it ${it.length}")
-    }
-}
